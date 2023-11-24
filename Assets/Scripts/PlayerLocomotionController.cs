8498631d3613f3f4dae3ab3d71a25d78ae516868
MrResistance <Paul1208Gush@Gmail.com>
2023-11-24 17:38:24 +0000

    Test

--- Assets/Scripts/PlayerLocomotionController.cs
+++ Assets/Scripts/PlayerLocomotionController.cs
@@ -5,27 +5,53 @@ public class PlayerLocomotionController : NetworkBehaviour
 {
     private void Start()
     {
-        if (isLocalPlayer && PlayerInputs.Instance != null)
+        if (isServer)
         {
-            PlayerInputs.Instance.OnMove -= OnMove;
-            PlayerInputs.Instance.OnMove += OnMove;
+            StartServer();
+        }
+        else
+        {
+            StartClient();
+        }
+    }
+
+    [Server]
+    private void StartServer()
+    {
+        
+    }
+
+    [Client]
+    private void StartClient()
+    {
+        if (PlayerInputs.Instance != null)
+        {
+            PlayerInputs.Instance.OnMove -= TryMove;
+            PlayerInputs.Instance.OnMove += TryMove;
         }
     }
 
     private void OnEnable()
     {
-        if (PlayerInputs.Instance != null)
+        if (isLocalPlayer && PlayerInputs.Instance != null)
         {
-            PlayerInputs.Instance.OnMove += OnMove;
+            PlayerInputs.Instance.OnMove += TryMove;
         }
     }
 
     private void OnDisable()
     {
-        PlayerInputs.Instance.OnMove -= OnMove;
+        PlayerInputs.Instance.OnMove -= TryMove;
     }
+
+    [Command]
     private void OnMove(Vector2 newVal)
     {
         Debug.Log(netIdentity + ": " + newVal);
+    }
+    private void TryMove(Vector2 newVal)
+    {
+        Debug.Log("Trying to move");
+        OnMove(newVal);
     }
 }
