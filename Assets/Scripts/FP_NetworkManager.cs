caab6860fa7fabc70b6fe9dee0fc7b82aad53a0b
RyanDWakeford <ryandwakeford@gmail.com>
2023-11-28 14:46:34 +0000

    RW - Player is removed from Lists

--- Assets/Scripts/FP_NetworkManager.cs
+++ Assets/Scripts/FP_NetworkManager.cs
@@ -3,13 +3,14 @@ using System.Diagnostics;
 using UnityEngine;
 public class FP_NetworkManager : NetworkManager
 {
+    [Server]
     public override void OnServerDisconnect(NetworkConnectionToClient conn)
     {
         for (int i = 0; i < conn.owned.Count; i++)
         {
-            if (conn.identity.TryGetComponent<Player>(out Player player))
+            if (conn.identity.TryGetComponent(out Player player))
             {
-                player.RequestRemovalFromList();
+                GameManager.Instance.RemovePlayerFromAllLists(player);
             }
             UnityEngine.Debug.Log("Player Disconnected: " + conn.address);
         }
